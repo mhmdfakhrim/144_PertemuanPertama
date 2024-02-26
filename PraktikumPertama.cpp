4825f56b7c3c01a2d8d886e9ed857c1451c859bd
mhmdfakhrim <muhfakhri4@gmail.com>
2024-02-26 15:33:37 +0700

    pemberian comment

--- PraktikumPertama.cpp
+++ PraktikumPertama.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main()
 {//begin
 
+    //numeric nPanjang,nLebar, nLuas
+    //display 'masukkan panjang'
+    //accept nPanjang
+    //display 'masukkan lebar'
+    //accept other
+    //compute nLuas = nPanjang * nLebar
+    //display 'luasnya adalah = ' * nLuas
+
     int nPanjang, nLebar, nLuas;
     cout << "Masukkan Panjang = ";
     cin >> nPanjang;
