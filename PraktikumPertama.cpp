524bd23cf4aac78a4352c136c03546c9fd9ed27b
mhmdfakhrim <muhfakhri4@gmail.com>
2024-02-26 15:13:43 +0700

    Add project files.

---
+++ PraktikumPertama.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int nPanjang, nLebar, nLuas;
+    cout << "Masukkan Panjang = ";
+    cin >> nPanjang;
+    cout << "Masukkan Lebar = ";
+    cin >> nLebar;
+    nLuas = nPanjang * nLebar;
+    cout << "Luasnya adalah = " << nLuas << endl;
+
+    system("pause");
+
+}
+
